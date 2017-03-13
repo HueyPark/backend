584451d7bf3e39f58c7bc337956d8a91074922f5
HueyPark <jaewan.huey.park@gmail.com>
2017-03-13 22:28:02 +0900

    Only 25 sample nodes left

--- src/framework/world/creater.py
+++ src/framework/world/creater.py
@@ -1,19 +1,16 @@
 from framework.database.models.node import Node
 from framework.database.session import session
 from framework.id_generator import id_generator
-from random import randint
 
 
 class Creater:
     def create_nodes(self):
-        for _ in range(10):
-            self.__create_random_node()
+        for x in range(5):
+            for y in range(5):
+                node = Node(id_generator.generate(), x * 100, y * 100)
+                session.add(node)
+        session.commit()
 
     def delete_nodes(self):
         session.query(Node).delete()
-        session.commit()
-
-    def __create_random_node(self):
-        node = Node(id_generator.generate(), randint(0, 1000), randint(0, 1000))
-        session.add(node)
         session.commit()
